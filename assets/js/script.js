1479746fe4d7489340eb2edabecb471b17e5bb09
Edivan <edivanlopescte@gmail.com>
2024-08-13 18:39:30 -0300

    Criando html, css e script parcial

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const textArea = document.querySelector(".texto-area");
+const mensagem = document.querySelector(".mensagem");
+
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+}
